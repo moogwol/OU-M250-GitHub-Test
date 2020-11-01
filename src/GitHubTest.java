f7c7807df704320468714f26d1de9323291a9500
Miguel Corkhill <moogwol@msn.com>
2020-11-01 18:28:27 +0100

    Changed the message back to hello world.

--- src/GitHubTest.java
+++ src/GitHubTest.java
@@ -1,7 +1,5 @@
 public class GitHubTest {
     public static void main(String[] args) {
-        System.out.println("This is a new branch of the Hello World project.");
-        System.out.println("It will be reviewed shortly and if it looks good,");
-        System.out.println("it will be merged with the main branch and then deleted.");
+        System.out.println("Hello World!");
     }
 }
