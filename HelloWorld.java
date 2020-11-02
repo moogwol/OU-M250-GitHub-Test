bac445d880030b2b56d237891347ae7fa7aef3b3
Hillig-dev <57876538+Hillig-dev@users.noreply.github.com>
2020-11-02 22:08:45 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package helloWorld_OU;
+
+public class HelloWorld {
+public static void main(String[] args) {
+    HelloWorldClass helloWorldObject = new HelloWorldClass();
+    helloWorldObject.helloWorldMethod();
+}
+}
